6a217895a00cc658c26b22503d73ddd0b5d449fb
prabhat34 <49153246+prabhat34@users.noreply.github.com>
2020-11-30 20:05:35 +0530

    updated practical-18.js

--- javascript/practical-18.js
+++ javascript/practical-18.js
@@ -1,130 +1,94 @@
-// Selectors
+var form = document.getElementById("form");
+var username = document.getElementById("username");
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+var confirmPassword = document.getElementById("confirm-password");
 
-const form = document.getElementById('form');
-const username= document.getElementById('username');
-const email= document.getElementById('email');
-const password1= document.getElementById('password1');
-const password2= document.getElementById('password2');
-const small= document.querySelector('small');
-
-
-
-// Event Listner
-
-//form submission
-// e tells us that which event is call
-form.addEventListener('submit', function(e){
-    // stop the default behaviour of submit
-    // means data is not refreshing anymore
-    //to stop submission
-    e.preventDefault();
-    // when data is  submitted we have to check all the inputs
-    //s o we make this function to check all the inputs
-    checkInput();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  checkValid();
 });
 
-
-
-// Function
-
-function checkInput()
-{
-    // trim removes the white spaces in the input 
-    const usernameValue = username.value.trim();
-    // console.log(usernameValue)
-    
-    const emailValue = email.value.trim();
-    const password1Value = password1.value.trim();
-    const password2Value = password2.value.trim();
-
-
-   
-    // -----------------Username-----------------
-    // if value in the username is empty
-    if(usernameValue===''){
-        // then showError function is called , which tells that error comes from username with this error message
-        showError(username, "Username can't be blank");
+function checkValid() {
+  var userMsg = document.getElementById("user-msg");
+  var nameFails = document.getElementById("name-fails");
+  var nameCheck = document.getElementById("name-check");
+  var emailError = document.getElementById("email-error");
+  var passwordError = document.getElementById("pass-error");
+  var emailFails = document.getElementById("email-fails");
+  var emailcheck = document.getElementById("email-check");
+  var passwordFails = document.getElementById("password-fails");
+  var passwordCheck = document.getElementById("password-check");
+  var confirm = document.getElementById("confirm-password");
+  var confirmError = document.getElementById("confirm-error");
+  var confirmFails = document.getElementById("confirm-fails");
+  var confirmCheck = document.getElementById("confirm-check");
+  if (username.value.trim() === "") {
+    username.style.borderBottom = "1px red solid";
+    userMsg.innerHTML = "* User Name cannot be Empty";
+    userMsg.style.visibility = "visible";
+    nameFails.style.visibility = "visible";
+  } else {
+    if (username.value.trim().match(/^[a-z0-9]+$/g)) {
+      userMsg.style.visibility = "hidden";
+      nameFails.style.visibility = "hidden";
+      nameCheck.style.visibility = "visible";
+      username.style.borderBottom = "1px green solid";
+    } else {
+      username.style.borderBottom = "1px red solid";
+      userMsg.innerHTML = "* User Name should only contain lower case with no special symbols";
+      userMsg.style.visibility = "visible";
+      nameFails.style.visibility = "visible";
+      nameCheck.style.visibility = "hidden";
     }
-    else if(!isUsernameValid(usernameValue))
-    {
-        showError(username, "Username can have small letters and underscores only");
+  }
+  if (email.value.trim() === "") {
+    email.style.borderBottom = "1px red solid";
+    emailError.innerHTML = "* Email cannot be Empty";
+    emailError.style.visibility = "visible";
+    emailFails.style.visibility = "visible";
+  } else {
+    if (email.value.trim().match(/^[\w-\.]+@([\w-]+\.)+com$/g)) {
+      emailError.style.visibility = "hidden";
+      emailFails.style.visibility = "hidden";
+      emailcheck.style.visibility = "visible";
+      email.style.borderBottom = "1px green solid";
+    } else {
+      email.style.borderBottom = "1px red solid";
+      emailError.innerHTML = "* Enter a valid email";
+      emailError.style.visibility = "visible";
+      emailFails.style.visibility = "visible";
+      emailcheck.style.visibility = "hidden";
     }
-    else {
-        showSuccess(username);
+  }
+  if (password.value.trim() === "") {
+    password.style.borderBottom = "1px red solid";
+    passwordError.innerHTML = "* Password cannot be Empty";
+    passwordError.style.visibility = "visible";
+    passwordFails.style.visibility = "visible";
+  } else {
+    passwordError.style.visibility = "hidden";
+    passwordFails.style.visibility = "hidden";
+    passwordCheck.style.visibility = "visible";
+    password.style.borderBottom = "1px green solid";
+  }
+  if (confirm.value.trim() === "") {
+    confirm.style.borderBottom = "1px red solid";
+    confirmError.innerHTML = "* Password cannot be Empty";
+    confirmError.style.visibility = "visible";
+    confirmFails.style.visibility = "visible";
+  } else {
+    if (confirm.value === password.value) {
+      confirmError.style.visibility = "hidden";
+      confirmFails.style.visibility = "hidden";
+      confirmCheck.style.visibility = "visible";
+      confirm.style.borderBottom = "1px green solid";
+    } else {
+      confirm.style.borderBottom = "1px red solid";
+      confirmError.innerHTML = "* Password Should be same as above";
+      confirmError.style.visibility = "visible";
+      confirmFails.style.visibility = "visible";
+      confirmCheck.style.visibility = "hidden";
     }
-
-
-
-    //-----------------email-----------------
-    if(emailValue==='')
-    {
-        showError(email, "Email Id can't be blank");
-    }
-    else if(!isEmailValid(emailValue))
-    {
-        showError(email, "Email Id is inavalid");
-    }
-    else{
-        showSuccess(email);
-    }
-
-
-    // ---------------password--------------
-    if(password1Value===''){
-        showError(password1, "Password can't be blank");
-    }
-    else {
-        showSuccess(password1);
-    }
-
-
-    //--------------conferm password------------
-    if(password2Value===''){
-        showError(password2, "Re-enter password");
-    }
-    else if(password2Value != password1Value)
-    {
-        showError(password2, "Password not matched!")
-    }
-    else {
-        showSuccess(password2);
-    }
-}
-
-
-
-function showError(input, msg)
-{
-    // parent node of input
-    const formControl = input.parentNode;
-    // add a new class of formControl
-    formControl.className = 'form-control error';
-    // formControl ke andar hme small ko select krna h 
-    const small= formControl.querySelector('small');
-    // set msg in small which is come from error
-    small.innerHTML= msg;
-}
-
-
-
-function showSuccess(input)
-{
-    // parent node of input
-    const formControl = input.parentNode;
-    // add a new class of formControl
-    formControl.className = 'form-control success';
-}
-
-
-
-function isEmailValid(email)
-{
-    // email validation regex
-    return /^([a-zA-Z0-9_\.\-\+]+)@([a-zA-Z0-9]+)\.([a-zA-Z]{2,3})$/.test(email);
-}
-
-function isUsernameValid(username)
-{
-    // username validation regex
-    return /^[a-z_]+$/.test(username);
+  }
 }
